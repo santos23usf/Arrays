1e15c6daeb380b688564d2b8eb1f35821cd42bcb
Catherine Santos <santos23@usf.edu>
2020-10-15 12:20:32 -0400

    array table

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 namespace Arrays
 {
@@ -6,7 +7,19 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           Console.WriteLine("Hello professor, this is an auto populated array for Project #3B!");
+
+            // Create the array and declarate the array's size and associate them to each other
+            const int SIZE = 25;
+            int[] numbersarray = new int[SIZE];
+            
+            // Iterate through the array using a For loop. Automatically populate array using a function and display values
+            for (int i = 0; i < SIZE; i++)
+            {
+                numbersarray[i] += (i+1);
+                Console.WriteLine("Element value = " + numbersarray[i]);
+            }
+            Console.ReadKey(true);
         }
     }
 }
